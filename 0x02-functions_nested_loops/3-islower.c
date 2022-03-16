fb2c8a9c0232729f636c731e63fbb104cd9a87df
leroy <45094643+leroy9387@users.noreply.github.com>
2022-03-17 01:44:24 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * _islower - prints 1 or 0 depending on input
- * @c: first parameter
- *
- * Description: prints all lowercase letters
- * Return: Always(0).
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
  */
-
 int _islower(int c)
+
 {
-	return (c >= 'a' && c <= 'z');
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
